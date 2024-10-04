1665a48ec01b20659ade3d75a1f411b9be3cc09a
abuhumayed <ahmedthanjim@hotmail.co.uk>
2024-10-04 10:32:55 +0100

    counter

--- src/components/ViewAllJobs.jsx
+++ src/components/ViewAllJobs.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const ViewAllJobs = () => {
+  const [count, setCount] = useState(0);
   return (
     <>
       <section className="m-auto max-w-lg my-10 px-6">
@@ -10,6 +12,10 @@ const ViewAllJobs = () => {
         >
           View All Jobs
         </Link>
+
+        <button className="px-5 border border-black mx-5 my-2">-</button>
+        <span>0</span>
+        <button className="px-5 border border-black mx-5">+</button>
       </section>
     </>
   );
